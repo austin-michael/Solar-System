9283b625448cafb390a94e0837c47bb03a7aabe2
austin-michael <austin.michaelwill@gmail.com>
2024-02-11 19:03:53 -0700

    Added plant array

--- src/script.js
+++ src/script.js
@@ -20,24 +20,61 @@ sun.scale.setScalar(5);
 
 scene.add(sun);
 
-// Create Earth
-const earthMaterial = new THREE.MeshBasicMaterial({
-  color: "blue",
-});
-
-const earth = new THREE.Mesh(sphereGeometry, earthMaterial);
-earth.position.x = 10;
-
-scene.add(earth);
-
-// Create Moon
-const moonMaterial = new THREE.MeshBasicMaterial({
-  color: "grey",
-});
-const moon = new THREE.Mesh(sphereGeometry, moonMaterial);
-moon.scale.setScalar(0.3);
-moon.position.x = 2;
-earth.add(moon);
+const planets = [
+  {
+    name: "Mercury",
+    radius: 0.5,
+    distance: 10,
+    speed: 0.01,
+    material: mercuryMaterial,
+    moons: [],
+  },
+  {
+    name: "Venus",
+    radius: 0.8,
+    distance: 15,
+    speed: 0.007,
+    material: venusMaterial,
+    moons: [],
+  },
+  {
+    name: "Earth",
+    radius: 1,
+    distance: 20,
+    speed: 0.005,
+    material: earthMaterial,
+    moons: [
+      {
+        name: "Moon",
+        radius: 0.3,
+        distance: 3,
+        speed: 0.015,
+      },
+    ],
+  },
+  {
+    name: "Mars",
+    radius: 0.7,
+    distance: 25,
+    speed: 0.003,
+    material: marsMaterial,
+    moons: [
+      {
+        name: "Phobos",
+        radius: 0.1,
+        distance: 2,
+        speed: 0.02,
+      },
+      {
+        name: "Deimos",
+        radius: 0.2,
+        distance: 3,
+        speed: 0.015,
+        color: 0xffffff,
+      },
+    ],
+  },
+];
 
 // initialize the camera
 const camera = new THREE.PerspectiveCamera(
